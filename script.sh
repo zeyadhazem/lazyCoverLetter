12e7fb71d2ab9eecaeed17b188d08f6554a084c5
Zeyad Saleh <zeyadhazemsaleh@gmail.com>
2017-02-28 17:50:28 -0500

    Cleanup

--- script.sh
+++ script.sh
@@ -5,25 +5,9 @@ MONTH=$(date +"%B")
 DAY=$(date +"%-d")
 YEAR=$(date +"%Y")
 
-read -p "Enter Company Name: "  COMPANY
+read -p "Enter Company Name: " COMPANY
 
-wday_q=s/_wday/
-wday_q=$wday_q$WDAY
-wday_q=$wday_q/g
-
-month_q=s/_month/
-month_q=$month_q$MONTH
-month_q=$month_q/g
-
-day_q=s/_day/
-day_q=$day_q$DAY
-day_q=$day_q/g
-
-year_q=s/_year/
-year_q=$year_q$YEAR
-year_q=$year_q/g
-
-sed -e $wday_q -e $month_q -e $day_q -e $year_q -e 's/<Company>/'"$COMPANY"'/g' <mycl.tex >modified.tex
+sed -e 's/<wday>/'"$WDAY"'/g' -e 's/<month>/'"$MONTH"'/g' -e 's/<day>/'"$DAY"'/g' -e 's/<year>/'"$YEAR"'/g' -e 's/<Company>/'"$COMPANY"'/g' <mycl.tex >modified.tex
 
 pdflatex -jobname mycl modified
 
