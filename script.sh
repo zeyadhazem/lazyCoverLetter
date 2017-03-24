8c44e89fed2bfa5dea5155bf48e2c2c75d33175c
Zeyad Saleh <zeyadhazemsaleh@gmail.com>
2017-03-23 23:30:43 -0400

    Added the option to input a position of interest + Cmd to cleanup the output

--- script.sh
+++ script.sh
@@ -6,9 +6,12 @@ DAY=$(date +"%-d")
 YEAR=$(date +"%Y")
 
 read -p "Enter Company Name: " COMPANY
+read -p "Enter position title: " POSITION
 
-sed -e 's/<wday>/'"$WDAY"'/g' -e 's/<month>/'"$MONTH"'/g' -e 's/<day>/'"$DAY"'/g' -e 's/<year>/'"$YEAR"'/g' -e 's/<Company>/'"$COMPANY"'/g' <mycl.tex >modified.tex
+sed -e 's/<wday>/'"$WDAY"'/g' -e 's/<month>/'"$MONTH"'/g' -e 's/<day>/'"$DAY"'/g' -e 's/<year>/'"$YEAR"'/g' -e 's/<Company>/'"$COMPANY"'/g' -e 's/<position>/'"$POSITION"'/g' <mycl.tex >modified.tex
 
 pdflatex -jobname mycl modified
+
+rm -rf *.log *.out *.aux modified.tex
 
 open mycl.pdf
